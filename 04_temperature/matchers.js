aa5ef92734dbd494088f8a46542ef5774cb55dba
Francesco Pezzella <franzpezzella@gmail.com>
2015-08-06 16:50:49 +0200

    Fix to Jasmine 2 syntax.

--- 04_temperature/matchers.js
+++ 04_temperature/matchers.js
@@ -1,8 +1,8 @@
 beforeEach(function() {
-  this.addMatchers({
+  jasmine.addMatchers({
     toBeCloseTo: function(expected, precision) {
 			precision = precision || 2;
-			var multiplier = Math.pow(10, precision);			
+			var multiplier = Math.pow(10, precision);
 			var actual = Math.round(this.actual * multiplier);
 			expected = Math.round(expected * multiplier);
 			return expected == actual;
